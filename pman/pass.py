c6a54ccf966fd54854cb6363c632c830113ea2ab
Sonal Santan <sonal.santan@amd.com>
2023-12-27 22:06:09 -0800

    pylint based code cleanup

--- pman/pass.py
+++ pman/pass.py
@@ -32,67 +32,75 @@ class PasswordManager:
     _SCHEMA = ['ORGANIZATION', 'URL', 'USERID', 'PASSWD', 'OTHERID1', 'OTHERID2', 'DATE',
                'KIND', 'NOTES']
 
-    def __init__(self, csvFileName = PASSCSV):
-        self._csvFileName = csvFileName
-        print(f"Initializing database from CSV file {self._csvFileName}")
-        self._passwordTable = []
-        with open(self._csvFileName, mode='r', encoding='utf8') as csvFileHandle:
-            reader = csv.DictReader(csvFileHandle, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+    def __init__(self, csv_file_name = PASSCSV):
+        """ PasswordManager class constructor """
+        self._csv_file_name = csv_file_name
+        print(f"Initializing database from CSV file {self._csv_file_name}")
+        self._password_table = []
+        with open(self._csv_file_name, mode='r', encoding='utf8') as csv_file_handle:
+            reader = csv.DictReader(csv_file_handle, delimiter=',', quoting=csv.QUOTE_MINIMAL)
             assert(reader.fieldnames == PasswordManager._SCHEMA)
             for row in reader:
-                self._passwordTable.append(row)
-        print(f"Found total {len(self._passwordTable)} records in the database")
+                self._password_table.append(row)
+        print(f"Found total {len(self._password_table)} records in the database")
 
-    def _getUpdateCSVFileName(self):
-        csvUpdateFileName = os.path.splitext(self._csvFileName)[0]
-        csvUpdateFileName += '.'
-        csvUpdateFileName += datetime.date.today().isoformat()
-        csvUpdateFileName += '.csv'
-        return csvUpdateFileName
+    def _get_update_csv_file_name(self):
+        """ Generate time indexed csv file name to store the updated database """
+        csv_update_file_name = os.path.splitext(self._csv_file_name)[0]
+        csv_update_file_name += '.'
+        csv_update_file_name += datetime.date.today().isoformat()
+        csv_update_file_name += '.csv'
+        return csv_update_file_name
 
-    def writeUpdateTable(self):
-        with open(self._getUpdateCSVFileName(), mode='w', encoding='utf8') as csvUpdateFileHandle:
-            writer = csv.DictWriter(csvUpdateFileHandle, PasswordManager._SCHEMA, restval = None,
-                                    delimiter=',', quoting=csv.QUOTE_MINIMAL)
+    def write_update_table(self):
+        """ Write out the updated databse to the new csv file """
+        with open(self._get_update_csv_file_name(), mode='w',
+                  encoding='utf8') as csv_update_file_handle:
+            writer = csv.DictWriter(csv_update_file_handle, PasswordManager._SCHEMA,
+                                    restval = None, delimiter=',', quoting=csv.QUOTE_MINIMAL)
             writer.writeheader()
-            for row in self._passwordTable:
+            for row in self._password_table:
                 writer.writerow(row)
 
-    def extractOrg(self, orgName):
-        pattern = re.compile(orgName, re.IGNORECASE)
-        orgList = []
-        for row in self._passwordTable:
+    def extract_org(self, org_name):
+        """ Look up the record(s) with matching org name """
+        pattern = re.compile(org_name, re.IGNORECASE)
+        org_list = []
+        for row in self._password_table:
             if (pattern.search(row['ORGANIZATION'])):
-                orgList.append(row)
-        return orgList
+                org_list.append(row)
+        return org_list
 
-    def askUserAndUpdate(self, row):
+    def ask_user_and_update(self, row):
+        """ Ask the user to provide updated record fields for a org """
         response = "no"
         while (response != "yes"):
             for key in list(row):
                 value = input(f"{key}: [{row[key]}] ")
                 if (len(value)):
                     row[key] = value
-            pp = pprint.PrettyPrinter(indent=4, sort_dicts=False)
-            pp.pprint(row)
+            pretty = pprint.PrettyPrinter(indent=4, sort_dicts=False)
+            pretty.pprint(row)
             response = input("Commit the above to the database? [yes/no] ")
 
-    def updateOrg(self, orgName):
-        orgList = self.extractOrg(orgName)
+    def update_org(self, org_name):
+        """ Either update an exisiting record or create a new record and commit to the database """
+        org_list = self.extract_org(org_name)
         row = None
-        if (len(orgList) > 1):
-            raise RuntimeError(f"Error: found more than one record with {orgName}")
-        if (len(orgList) == 1):
-            row = orgList[0]
+        if (len(org_list) > 1):
+            raise RuntimeError(f"Error: found more than one record with {org_name}")
+        if (len(org_list) == 1):
+            row = org_list[0]
         else:
             values = ['None'] * len(PasswordManager._SCHEMA)
             row = dict((zip(PasswordManager._SCHEMA, values)))
-            row['ORGANIZATION'] = orgName
+            row['ORGANIZATION'] = org_name
             row['DATE'] = datetime.date.today().isoformat()
-            self._passwordTable.append(row)
-        self.askUserAndUpdate(row)
+            self._password_table.append(row)
+        self.ask_user_and_update(row)
 
-def parseCommandLine(args):
+def parse_command_line(args):
+    """ Command line parsing helper routine """
     msg = "Lookup the password for the organization requested"
     parser = argparse.ArgumentParser(description = msg, exit_on_error = False)
     parser.add_argument('-o', '--org', dest = 'oname', nargs = 1, required=True)
@@ -102,26 +110,27 @@ def parseCommandLine(args):
     return parser.parse_args(args[1:])
 
 def main(args):
+    """ Main entry point function """
     try:
-        argtab = parseCommandLine(args)
-        pm = PasswordManager(argtab.fname)
-        rows = pm.extractOrg(argtab.oname[0])
-        pp = pprint.PrettyPrinter(indent=4, sort_dicts=False)
-        pp.pprint(rows)
+        argtab = parse_command_line(args)
+        pman = PasswordManager(argtab.fname)
+        rows = pman.extract_org(argtab.oname[0])
+        pretty = pprint.PrettyPrinter(indent=4, sort_dicts=False)
+        pretty.pprint(rows)
 
         if (argtab.update):
-            pm.updateOrg(argtab.oname[0])
-            pm.writeUpdateTable()
+            pman.update_org(argtab.oname[0])
+            pman.write_update_table()
         return 0
 
-    except OSError as o:
-        print(o)
-        return o.errno
-    except AssertionError as a:
-        print(a)
+    except OSError as oerr:
+        print(oerr)
+        return oerr.errno
+    except AssertionError as aerr:
+        print(aerr)
         return 1
-    except Exception as e:
-        print(e)
+    except Exception as eerr:
+        print(eerr)
         return 1
 
 if __name__ == '__main__':
