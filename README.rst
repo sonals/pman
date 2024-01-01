b26014ba0c41e7fef0dbeb38f2134f054519cfd3
Sonal Santan <sonal.santan@amd.com>
2023-12-31 22:02:45 -0800

    Add a selftest for CryptIOBroker
    Serialize and store the random salt as first 16 bytes in the file storage and retrieve it back when reading from the file

--- README.rst
+++ README.rst
@@ -10,6 +10,18 @@ pman.pass
 
 A simple command line based password manager written in python. This is
 work-in-progress.
-TODO: Add support for encryption of the csv database
 
+TODO:
+*****
+
+1. Clean up CryptIOBroker.py code base
+   * Remove random cruft
+   * Add a built-in self test
+   * Validate corner cases
+2. Define a schema for storing (and retrieving) the following in the file:
+   * random salt
+   * user name
+   * date stamp
+   * encrypted data
+3.
 -------------------------------------------------------------------------------
---
+++ pman/CryptIOBroker.py
@@ -0,0 +1,131 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+# Copyright (C) 2023 sonal.santan@gmail.com
+#
+
+import base64
+import cryptography.fernet
+import cryptography.hazmat.primitives.hashes
+import cryptography.hazmat.primitives.kdf.pbkdf2
+import io
+import os
+import pprint
+import csv
+
+class CryptIOBroker(io.StringIO):
+    """
+    A simple wrapper around io.StringIO module. Provides transparent encryption of
+    all writes and inline decryption of all reads. The encrypted data is stored or
+    retrieved from the file provided.
+    The password provided in the class constructor is stretched and then paired with
+    a salt following the Python cryptography example described here--
+    https://cryptography.io/en/latest/hazmat/primitives/key-derivation-functions/code
+    The module stages all data in the backing storage of base class, io.StringIO.
+    1. Base class methods write() and writelines() store the unencrypted contents into the
+       backing storage provided by the base class. The overridden close() method is used
+       to encrypt the stored data and then flush it to the file.
+    2. Base class methods read()/readlines() retieve decrypted contents from the backing
+       storage provided by the base class. The backing storage is fully populated by reading
+       all data from the file and decrypting it in the module constructor.
+    """
+
+    _SELFTESTFILE = "/tmp/ducati-store.dat"
+
+    @classmethod
+    def _getengine(cls, password, salt):
+        kdf = cryptography.hazmat.primitives.kdf.pbkdf2.PBKDF2HMAC(
+            algorithm=cryptography.hazmat.primitives.hashes.SHA256(),
+            length=32,
+            salt=salt,
+            iterations=480000)
+
+        key = base64.urlsafe_b64encode(kdf.derive(password.encode('utf-8')))
+        return cryptography.fernet.Fernet(key)
+
+
+    def __init__(self, password, mode, filename):
+        """ CryptIOBroker class constructor """
+        self._mode = mode
+        self._filename = filename
+        # Only exclusive read or write operation is supported; 'rw' is not supported
+        assert (self._mode == 'r') or (self._mode == 'w'), \
+               "Either text read or text write mode is supported"
+
+        if (self._mode == 'r'):
+            with open(self._filename, mode='rb') as filehandle:
+                self._salt = filehandle.read(16)
+                self._engine = CryptIOBroker._getengine(password, self._salt)
+                bindata = filehandle.read()
+                contents = self._engine.decrypt(bindata)
+            super().__init__(contents.decode('utf-8'))
+        else:
+            self._salt = os.urandom(16)
+            self._engine = CryptIOBroker._getengine(password, self._salt)
+            super().__init__()
+
+    def close(self):
+        if (self._mode == 'w'):
+            contents = super().getvalue()
+            with open(self._filename, mode='wb') as filehandle:
+                bindata = self._engine.encrypt(contents.encode('utf-8'))
+                filehandle.write(self._salt)
+                filehandle.write(bindata)
+        super().close()
+
+    @classmethod
+    def selftest(cls):
+        c = CryptIOBroker("sonal-pass", 'w', cls._SELFTESTFILE)
+        c.writelines("hello\n")
+        c.writelines("bye\n")
+        c.close()
+
+        d = CryptIOBroker("sonal-pass", 'r', cls._SELFTESTFILE)
+        r = d.readlines()
+        pretty = pprint.PrettyPrinter(indent=4, sort_dicts=False)
+        pretty.pprint(r)
+        d.close()
+
+
+_SCHEMA = ['ORGANIZATION', 'URL', 'USERID', 'PASSWD', 'OTHERID1', 'OTHERID2', 'DATE',
+           'KIND', 'NOTES']
+def etest():
+    key = cryptography.fernet.Fernet.generate_key()
+    f = cryptography.fernet.Fernet(key)
+    edata = f.encrypt(b"A really secret message. Not for prying eyes.")
+    ddata = f.decrypt(edata)
+    print(ddata)
+
+def keytest():
+    password = b"password"
+    salt = os.urandom(16)
+    kdf = cryptography.hazmat.primitives.kdf.pbkdf2.PBKDF2HMAC(
+        algorithm=cryptography.hazmat.primitives.hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=480000)
+
+    key = base64.urlsafe_b64encode(kdf.derive(password))
+    f = cryptography.fernet.Fernet(key)
+
+    data = None
+    with open("/tmp/password.csv", mode='r', encoding='utf8') as csv_file_handle:
+        data = csv_file_handle.read()
+
+    reader = csv.DictReader(io.StringIO(data), delimiter=',', quoting=csv.QUOTE_MINIMAL)
+    assert(reader.fieldnames == _SCHEMA)
+    pretty = pprint.PrettyPrinter(indent=4, sort_dicts=False)
+    for row in reader:
+        pretty.pprint(row)
+
+    token = f.encrypt(b"Secret message!")
+    data = f.decrypt(token)
+
+
+    output = io.StringIO()
+
+def brokertest():
+    CryptIOBroker.selftest()
+
+if __name__ == '__main__':
+    brokertest()
+    keytest()
